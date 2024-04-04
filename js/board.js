6c6cb4f941c821b58aa87e1bf7c8c2f4518cdd2b
Mario Markwart <mario.markwart@gmail.com>
2024-04-04 12:58:21 +0200

    board.js cards[]

---
+++ js/board.js
@@ -0,0 +1,23 @@
+let cards = [
+    {
+        'topic':'User Story',
+        'title':'Kochwelt Page & Recipe Recommender',
+        'text': 'Build start page with recipe recommendation',
+        'subtasks': 2,
+        'assignedTo': ['SS','MM',"AS"]
+    },
+    {
+        'topic':'User Story',
+        'title':'Kochwelt Page & Recipe Recommender',
+        'text': 'Build start page with recipe recommendation',
+        'subtasks': 2,
+        'assignedTo': ['SS','MM',"AS"]
+    },
+    {
+        'topic':'User Story',
+        'title':'Kochwelt Page & Recipe Recommender',
+        'text': 'Build start page with recipe recommendation',
+        'subtasks': 2,
+        'assignedTo': ['SS','MM',"AS"]
+    }
+]
