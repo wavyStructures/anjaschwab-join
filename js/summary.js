8e43346abaf6c724540f4f76ea457f718d65b395
wavyStructures <anja_schwab@gmx.de>
2024-04-06 09:18:08 +0200

    anja summary dateBox

--- js/summary.js
+++ js/summary.js
@@ -1,3 +1,12 @@
+let today = new Date();
+let options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+};
+let formattedDate = today.toLocaleDateString("en-US", options);
+
+document.querySelector('.date').textContent = formattedDate;
 
 
 
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-async function init(){
+async function init() {
   renderHeader();
   renderNavigation();
   renderBoard();
@@ -9,67 +9,78 @@ async function init(){
  * Includes HTML-Files into container containing "w3-include-html"-attribute 
  */
 async function includeHTML() {
-	let includeElements = document.querySelectorAll("[w3-include-html]");
-	for (let i = 0; i < includeElements.length; i++) {
-		const element = includeElements[i];
-		file = element.getAttribute("w3-include-html");
-		let resp = await fetch(file);
-		if (resp.ok) {
-			element.innerHTML = await resp.text();
-		} else {
-			element.innerHTML = "Page not found";
-		}
-	}
+  let includeElements = document.querySelectorAll("[w3-include-html]");
+  for (let i = 0; i < includeElements.length; i++) {
+    const element = includeElements[i];
+    file = element.getAttribute("w3-include-html");
+    let resp = await fetch(file);
+    if (resp.ok) {
+      element.innerHTML = await resp.text();
+    } else {
+      element.innerHTML = "Page not found";
+    }
+  }
 }
 
 
-function getDiv(id){
+function getDiv(id) {
   let content = document.getElementById(id);
   return content;
 }
 
 
-function renderNavigation(){
+function renderNavigation() {
   let content = getDiv('navigation-container');
   content.innerHTML = renderNavigationHTML();
 }
 
 
-function renderHeader(){
-let content = getDiv('header');
-content.innerHTML = renderHeaderHTML();
+function renderHeader() {
+  let content = getDiv('header');
+  content.innerHTML = renderHeaderHTML();
 }
 
 
-function renderSummary(){
+function renderSummary() {
   let content = getDiv('main');
   content.innerHTML = renderSummaryHTML();
 }
 
 
-function renderAddTask(){
+function renderAddTask() {
   let content = getDiv('main');
   content.innerHTML = renderAddTaskHTML();
 }
 
 
-function renderBoard(){
+function renderBoard() {
   let content = getDiv('main');
   content.innerHTML = renderBoardHTML();
 }
 
 
-function renderContacts(){
+function renderContacts() {
   let content = getDiv('main');
   content.innerHTML = renderContactsHTML();
 }
 
 
+let today = new Date();
+let options = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric'
+};
+let formattedDate = today.toLocaleDateString("en-US", options);
 
+/**
+ * summary current date insertion into template
+ */
+document.querySelector('.date').textContent = formattedDate;
 
 // /**
-//  * 
-//  * @param {String} folderPath 
+//  *
+//  * @param {String} folderPath
 //  */
 // function dynamicallyLoadScriptsFromFolder(folderPath) {
 //   var scripts = [
