869235a5cca9dae9d5eed25b35e987e3db26d8b9
Mario Markwart <mario.markwart@gmail.com>
2024-04-18 15:52:29 +0200

    Mario: addTask.js added color on prio when button is active

--- js/addTask.js
+++ js/addTask.js
@@ -2,3 +2,24 @@ function addTask(category){
     console.log("Task added to:", category);
     console.log("Add function to addTask-Buttons");
 }
+
+function setPriority(chosenPriority){
+
+    let prioBtns = document.getElementsByClassName("addTaskPriorityButton");
+    console.log(prioBtns);
+    console.log( 'addTaskPriorityButton' + chosenPriority);
+
+    Array.from(prioBtns).forEach(btn => {
+        if (btn.id == 'addTaskPriorityButton' + chosenPriority){
+            switch (chosenPriority) {
+                case "Urgent": btn.style = "background-color: var(--clr-prio-urgent)"; break;
+                case "Medium": btn.style = "background-color: var(--clr-prio-medium)"; break;
+                case "Low": btn.style = "background-color: var(--clr-prio-low)"; break;
+            }
+        }
+        else{
+            btn.style = "background-color: none";
+        }
+        
+    });
+}
