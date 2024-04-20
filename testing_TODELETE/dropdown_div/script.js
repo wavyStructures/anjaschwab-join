9bd5e3a8d34b425ef9da0402979554cfa30feb09
Mario Markwart <mario.markwart@gmail.com>
2024-04-20 21:41:26 +0200

    Mario: dropdownDiv

---
+++ testing_TODELETE/dropdown_div/script.js
@@ -0,0 +1,10 @@
+function renderArrow(){
+    let customArrow = document.getElementById('custom-arrow')
+    let arrowImg = customArrow.childNodes[1];
+    
+    arrowImg.dataset.direction == "down"
+    ? arrowImg.dataset.direction = "up"
+    : arrowImg.dataset.direction = "down"
+
+    arrowImg.src = `../../assets/img/arrow_${arrowImg.dataset.direction}.png`
+}
