62530a38d84ca53da82d1eb2027433010c7be11b
wavyStructures <anja_schwab@gmx.de>
2024-04-20 11:24:51 +0200

    anja summary greeting css js third commit [data-user-type=user] #usernameForGreeting

--- js/summary.js
+++ js/summary.js
@@ -10,23 +10,41 @@ function summaryInit() {
  * @returns void
  */
 
-// function greeting(name) {
-const isLoggedIn = false;        //TODO is loggedIn setting
-console.log("isLoggedIn: ", isLoggedIn);
 
-const h1GreetingUser = document.getElementById("h1GreetingUser");
-const usernameForGreeting = document.getElementById("usernameForGreeting");
+document.addEventListener("DOMContentLoaded", function () {
+    const isLoggedIn = true;
+    console.log("isLoggedIn: ", isLoggedIn);
+
+    const h1GreetingUser = document.getElementById("h1GreetingUser");
+    const usernameForGreeting = document.getElementById("usernameForGreeting");
+
+    if (isLoggedIn) {
+        h1GreetingUser.dataset.userType = "user";
+        usernameForGreeting.innerText = `, ${currentUser.name}`;
+    } else {
+        h1GreetingUser.dataset.userType = "guest";
+        usernameForGreeting.innerText = '';
+    }
+});
 
 
-if (isLoggedIn) {
-    h1GreetingUser.dataset.userType = "user";
-    usernameForGreeting.innerText = `, ${currentUser.name}`;  //TODO currentUser anlegen
-} else {
-    h1GreetingUser.dataset.userType = "guest";
-    usernameForGreeting.innerText = '';
-}
+// // function greeting(name) {
+// const isLoggedIn = false;        //TODO is loggedIn setting
+// console.log("isLoggedIn: ", isLoggedIn);
 
+// const h1GreetingUser = document.getElementById("h1GreetingUser");
+// const usernameForGreeting = document.getElementById("usernameForGreeting");
+
+
+// if (isLoggedIn) {
+//     h1GreetingUser.dataset.userType = "user";
+//     usernameForGreeting.innerText = `, ${currentUser.name}`;  //TODO currentUser anlegen
+// } else {
+//     h1GreetingUser.dataset.userType = "guest";
+//     usernameForGreeting.innerText = '';
 // }
+
+// // }
 
 
 
