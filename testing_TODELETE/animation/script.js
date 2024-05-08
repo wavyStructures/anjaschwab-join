a7dbddf3c224c055beb27470fb3b478424ba9a69
Mario Markwart <mario.markwart@gmail.com>
2024-05-08 08:01:43 +0200

    Mario: testing animation

--- testing_TODELETE/animation/script.js
+++ testing_TODELETE/animation/script.js
@@ -1,11 +1,23 @@
 let isMoved = false;
 
-function moveDiv(side) {
-    console.log('move-to-' + side);
-    let div = document.getElementById("innerContainer");
-    if (side == 'left')
-    side == 'left' ? div.classList.remove('move-to-right') : div.classList.remove('move-to-left');
-    side == 'right' ? div.classList.remove('move-to-left') : div.classList.remove('move-to-right');
-    div.classList.add('move-to-' + side)
+function moveDiv() {
 
+    let div = document.getElementById("innerContainer");
+
+    // side == 'right' ? div.classList.remove('move-to-left') : div.classList.remove('move-to-right');
+    // side == 'left' ? div.classList.remove('move-to-right') : div.classList.remove('move-to-left');
+    
+    setTimeout(() => {
+        console.table('begin outerTimeout');
+        div.classList.add('move-to-right')
+        
+        setTimeout(() => {
+            console.log('begin innerTimeout');
+            div.classList.remove('move-to-right');
+            div.classList.add('move-to-left');
+            
+        },2000),1000}
+    );
+    div.classList.remove('move-to-left');
+    console.log('end timeOuts');
 }
