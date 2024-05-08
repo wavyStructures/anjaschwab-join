aff0ce09e72ae8e350b28d5673b60ae9513bb3fc
Mario Markwart <mario.markwart@gmail.com>
2024-05-08 10:10:52 +0200

    Mario: testing animation

--- testing_TODELETE/animation/script.js
+++ testing_TODELETE/animation/script.js
@@ -21,3 +21,24 @@ function moveDiv() {
     div.classList.remove('move-to-left');
     console.log('end timeOuts');
 }
+
+
+function randomize(){
+    let div = document.getElementById("innerContainer");
+    div.setAttribute('onmouseover', '')
+    let [newWidth, newHeight] = getRandomInt(window.innerWidth - div.offsetWidth/2, window.innerHeight - div.offsetHeight/2);
+    
+    let offsetTopOld = div.getBoundingClientRect().top;
+    let offsetLeftOld = div.getBoundingClientRect().left;
+
+    div.style.transform = `translate(${newWidth}px, ${newHeight}px)`;
+    div.animate([{transform: `translate(${offsetLeftOld}px, ${offsetTopOld}px)`},{transform: `translate(${newWidth}px, ${newHeight}px)`}], {duration: 500, fill: "forwards"}, {animationTimingFunction: 'ease-in-out'});
+    div.setAttribute('onmouseover', 'randomize()')
+}
+
+function getRandomInt(maxWidth, maxHeight) {
+    let width = Math.floor(Math.random() * maxWidth);
+    let height = Math.floor(Math.random() * maxHeight);
+    
+    return [width, height]
+  }
